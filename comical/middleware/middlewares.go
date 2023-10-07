f49914f405499dc09e68a633ce69eeb752432ea0
kiritoxkiriko <wangxinmeng1997@hotmail.com>
2023-10-07 21:04:43 +1100

    feat(middleware): add a new middleware header checker

--- comical/middleware/middlewares.go
+++ comical/middleware/middlewares.go
@@ -1,11 +1,13 @@
 package middleware
 
 import (
+	"fmt"
 	"github.com/kiritoxkiriko/comical"
 	"log"
 	"time"
 )
 
+// Logger add logger for each request
 func Logger(logger *log.Logger) comical.HandlerFunc {
 	if logger == nil {
 		logger = log.Default()
@@ -15,6 +17,18 @@ func Logger(logger *log.Logger) comical.HandlerFunc {
 		c.Next()
 		diff := time.Since(tStart)
 		log.Printf("[%d] %s in %v", c.StatusCode, c.Req.RequestURI, diff)
+	}
+	return handler
+}
+
+// HeaderChecker check header for each request, if not have, return http 400
+func HeaderChecker(key, value string) comical.HandlerFunc {
+	handler := func(c *comical.Context) {
+		val := c.Req.Header.Get(key)
+		if val != value {
+			c.Fail(400, fmt.Sprintf("header %s has wrong value", key))
+		}
+		c.Next()
 	}
 	return handler
 }
